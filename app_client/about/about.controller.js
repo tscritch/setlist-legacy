1cc774729152184f088bb57abc932193df546c3f
Tad Scritchfield <tadscritch@gmail.com>
2017-08-01 16:17:10 -0700

    add about page

---
+++ app_client/about/about.controller.js
@@ -0,0 +1,12 @@
+(function() {
+
+  'use strict';
+
+  angular.module('setlist').controller('aboutController', aboutController);
+
+  function aboutController() {
+    var vm = this;
+
+  }
+
+})();
